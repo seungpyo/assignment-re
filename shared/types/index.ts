d33a07438ed774dc13ffeb104af338ca61809c76
Seungpyo Hong <spkbk98@gmail.com>
2024-06-01 15:34:52 +0900

    added types

--- shared/types/index.ts
+++ shared/types/index.ts
@@ -4,3 +4,28 @@ export interface User {
   email: string;
   password: string;
 }
+
+export interface Channel {
+  id: string;
+  name: string;
+  participants: User[];
+  messages: Message[];
+}
+
+export interface Message {
+  sender: User;
+  content: string;
+  createdAt: string;
+}
+
+export interface Token {
+  id: string;
+  expiresAt: string;
+}
+
+export interface DB {
+  users: User[];
+  channels: Channel[];
+  messages: Message[];
+  tokens: Token[];
+}
