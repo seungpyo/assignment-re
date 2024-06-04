c1ef15058ec3426343d6cebc4211934d4ec1d97b
Seungpyo Hong <spkbk98@gmail.com>
2024-06-04 23:12:11 +0900

    fixed https port

--- client/src/constants.tsx
+++ client/src/constants.tsx
@@ -1,7 +1,8 @@
 export const mode = "remote";
 const host = "192.168.55.243";
 export const apiUrl = `https://${host}:443`;
-export const webSocketUrl = `wss://${host}:5001`;
+// export const webSocketUrl = `wss://${host}:5001`;
+export const webSocketUrl = `wss://${host}:443`;
 
 // export const mode = "localhost";
 // const host = "localhost";
